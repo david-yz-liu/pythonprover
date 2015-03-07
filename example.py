c7b99fd7cc4fa397d00530de97c7b7627096af27
LLay <lay.lyon@gmail.com>
2015-03-07 18:47:34 -0500

    Variable dictionary - keeps track variables and their relative value, post-condition checking, arbitrary number of arguments to 'assert' or 'requires, dynamic variable instantiation upon reading functionDef parameters

--- example.py
+++ example.py
@@ -1,7 +1,8 @@
 # requires(f(x, y) > 0)
 def f(x, y):
-	requires(x > 4)
-	assures(x > 0)
-	x = x - 5
+	requires(x == 5, y > 0)
+	x = x - 3 # modification of existing var
+	t, y = -1 # multiple assignments on one line (and declaration of new var)
+	assures(x > 0, y == -1)
 	return x
 
--- prover.py
+++ prover.py
@@ -3,6 +3,7 @@ import astpp
 import sys
 from ast import *
 import imp
+import re
 # import z3
 # from z3 import *
 # import codegen 
@@ -13,7 +14,7 @@ codegen = imp.load_source('codegen', '/Library/Frameworks/Python.framework/Versi
 
 # Z3
 sys.path.append('/Library/Frameworks/Python.framework/Versions/3.4/lib/Python3.4/z3/bin/')
-z3 = imp.load_source('z3', '/Library/Frameworks/Python.framework/Versions/3.4/lib/Python3.4/z3/bin/z3.py')
+z3 = imp.load_source('z3.py', '/Library/Frameworks/Python.framework/Versions/3.4/lib/Python3.4/z3/bin/z3.py')
 
 s = z3.Solver()
 # Dictionary containing the name of the user's variable as a key
@@ -24,7 +25,6 @@ vars_py_to_z3 = {}
 
 z3_funcs = {}
 
-Globals = {}
 
 ''' return the ast.Return node of the given ast.FunctionDef node, if there is one'''
 def get_return(node):
@@ -48,27 +48,7 @@ def get_func_args(node):
 
     return args
 
-class AssertCmpTransformer(ast.NodeTransformer):
-    """Transform 'assert a==b' into 'assert_equal(a, b)'
-    """
-    def visit_Assert(self, node):
-        print ("Preforming AssertCmpTransformer")
-        if isinstance(node.test, ast.Compare) and \
-                len(node.test.ops) == 1 and \
-                isinstance(node.test.ops[0], ast.Eq):
-            call = ast.Call(func=ast.Name(id='assert_equal', ctx=ast.Load()),
-                            args=[node.test.left, node.test.comparators[0]],
-                            keywords=[])
-            # Wrap the call in an Expr node, because the return value isn't used.
-            newnode = ast.Expr(value=call)
-            ast.copy_location(newnode, node)
-            ast.fix_missing_locations(newnode)
-            print ("SUCCESSFUL ASSERT")
-            return newnode
-        
-        # Return the original node if we don't want to change it.
-        return node
-
+''' Unnecessary - remove later'''
 class RequiresTransformer(ast.NodeTransformer):
 
     '''Transform 'requires(...)' into 's.add(...)' '''
@@ -90,6 +70,7 @@ class RequiresTransformer(ast.NodeTransformer):
         # Return the original node if we don't want to change it.
         return node
 
+''' That main thing '''
 class Z3Visitor(ast.NodeVisitor):
 
     def visit_Expr(self, node):
@@ -97,28 +78,50 @@ class Z3Visitor(ast.NodeVisitor):
         if isinstance(node.value, ast.Call) and \
             isinstance(node.value.func, ast.Name):
             if node.value.func.id == 'requires':
-                # Uncompile the body of 'requires(body)'. Call s.add(body)
-                # Currenly only handles a single call in 'body', though handling
-                # multiple arguments is possible. eg. assures(body1, body2, ...)
-                body = codegen.to_source(node.value.args[0])
-                x = z3.Int('x') # Need to get this dynamically
-                eval("s.add("+body+")")
-
-                print ("Expr (req):", codegen.to_source(node))
+                # Uncompile 'bodyx' in 'requires(body1, body2, ...) for all x
+                for condition in node.value.args:
+                    body = codegen.to_source(condition)
+                    eval("s.add"+body)
+                    print ("s.add"+body)
 
             elif node.value.func.id == 'assures':
-                # Uncompile the body of 'requires(body)'. Call s.add(body)
-                body = codegen.to_source(node.value.args[0])
-                x = z3.Int('x')
-                eval("s.add("+body+")")
+                # Uncompile 'bodyx' in 'assures(body1, body2, ...) for all x
+                for condition in node.value.args:
+                    body = codegen.to_source(condition)
 
-                print ("Expr (ass):", codegen.to_source(node))
+                    # Substitute variables in body with the corresponding value
+                    # found in the global variable dictionary
+                    for key in vars_py_to_z3:
+                        body = re.sub(re.compile(key), vars_py_to_z3[key], body)
+
+                    eval("s.add"+body)
+                    print ("s.add"+body)
         else:
             print ("Expr:", codegen.to_source(node))
 
-    ''' Will be used to execute z3 ...'''
+    ''' Update/Add to the vars_py_to_z3 dictionary to reflect the latest assignment'''
     def visit_Assign(self, node):
         print ("Assignment:", codegen.to_source(node))
+
+        targets = node.targets[0] # left hand side (Name or Tuple obj)
+        value = node.value # right hand side
+
+        if isinstance(targets, ast.Tuple): # multiple targets
+            for target in targets.elts:
+                if target.id not in vars_py_to_z3: # add
+                    vars_py_to_z3[target.id] = codegen.to_source(node.value)
+                else: # update
+                    old_val = vars_py_to_z3[target.id] 
+                    new_val = codegen.to_source(node.value)
+                    updated_val = re.sub(re.compile(target.id), "("+new_val+")", old_val)
+                    vars_py_to_z3[target.id] = updated_val
+
+        else : # only one target
+            old_val = vars_py_to_z3[targets.id] 
+            new_val = codegen.to_source(node.value)
+            updated_val = re.sub(re.compile(targets.id), "("+new_val+")", old_val)
+            
+            vars_py_to_z3[targets.id] = updated_val
 
     ''' Will be used to check for calls to functions we have instantiated z3
         equivalents to, and call those equivalents with the given arguments'''
@@ -128,27 +131,24 @@ class Z3Visitor(ast.NodeVisitor):
     def visit_FunctionDef(self, node):
         
         print ("Func:", codegen.to_source(node))
-        mylocals = {}
-        arg_list = get_func_args(node)
-        for arg in arg_list:
-            # Define each parameter. (Need to check if already declared in global scope?)
-            exec(arg+" = z3.Int('"+arg+"')")
-            # Add variable to global scope dictionary (make local?)
 
-        # x = z3.Int('x')
+        arg_list = get_func_args(node)
+
+        for arg in arg_list:
+            # Declare each parameter as a global variable
+            exec("global "+arg)
+            exec(arg+" = z3.Int('"+arg+"')", globals())
+
+            # Add the parameter to our global variable dictionary
+            vars_py_to_z3[arg] = arg
 
         # create z3 function def (need to dynamically fill in parameters)
         # how to infer return type?
-        f = z3.Function('f', z3.IntSort(), z3.IntSort(), z3.IntSort())
-        return_node = get_return(node)
+        # f = z3.Function('f', z3.IntSort(), z3.IntSort(), z3.IntSort())
+        # exec(node.name+" = z3.Funciton('"node.name"', "+" z3.IntSort(),"*len(arg_list)+"return")
+        # return_node = get_return(node)
 
-        # Post-condition using function body
-        # if var in post cond is modified in funciton (ie. assigned new val)
-        # then substitute assignment for var in post condition
-        # assures(x > 0) and x = x - 5:
-        s.add(x - 5 > 0)
-
-        # use this to visit function body and build z3 representation
+        # Visit function body and build z3 representation
         for elem in node.body:
             Z3Visitor().visit(elem)
 
@@ -171,13 +171,11 @@ print (astpp.dump(tree))
 Z3Visitor().visit(tree)
 
 result = s.check()
-print ("satisfiable:", result)
+print ("RESULT:", result)
 
-if (result):
-    pass
-    # m = s.model()
-    # print(m)
-    # print ("f(x) =", m.evaluate(f(x, y))) # f out of scope (use global dictionary?)
+# if (result):
+#     m = s.model()
+#     print("Model: ", m)
 
 # Testing validity: Requires building a function of the form:
 '''
